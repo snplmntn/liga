3087263b1641786f5cd34471812663c9098979d3
Sean <2136seanpaul@gmail.com>
2024-11-21 22:51:13 +0800

    Frontend Update

--- client/src/Pages/landing.jsx
+++ client/src/Pages/landing.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "../../src/Pages/styles/landing.css";
+import Nav from "../components/nav";
+import Result from "../components/result";
 
 export default function LandingPage() {
   return (
     <div>
-      <p className="test">test</p>
+      <Nav />
+
+      <div className="title section">
+        <Result />
+      </div>
+      {/* 
+      <div className="latest-update section">
+        <p>latest update</p>
+      </div> */}
     </div>
   );
 }
